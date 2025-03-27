ceef567403cc6e2b36afe84e54f4c89b90bc57dc
ok-dom <44346151+ok-dom@users.noreply.github.com>
2025-03-27 14:38:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // Массив с путями к изображениям
 const images = [
-    'images/image1.jpg',
-    'images/image2.jpg',
-    'images/image3.jpg',
+    'images/1.png',
+    'images/2.png',
+    'images/3.png',
+    'images/4.png',
     // Добавьте сюда пути к другим изображениям
 ];
 
